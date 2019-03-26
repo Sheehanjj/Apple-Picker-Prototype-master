a21140a0fe9b4fac03ab43dcc44596c1e1ed4355
John Sheehan <sheehanj@sunyulster.edju>
2019-03-26 11:18:34 -0400

    Updated Basket and Mouse Movement

---
+++ Apple-Picker-Prototype-master/Assets/Basket.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Basket : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Gets the current position of the mouse
+		Vector3 mousePos2D = Input.mousePosition;
+
+		// Sets how far to push the mouse into 3D
+		mousePos2D.z = -Camera.main.transform.position.z;
+
+		// Convert the point from 2D scren space into 3D game world space
+		Vector3 mousePos3D = Camera.main.ScreenToWorldPoint (mousePos2D);
+
+		// Move the basket based on the mouse
+		Vector3 pos = this.transform.position;
+		pos.x = mousePos3D.x;
+		this.transform.position = pos; // move the basket
+		
+	}
+
+	void OnCollisionEnter(Collision coll){
+		// something hit the basket what was it?
+		GameObject collidedWith = coll.gameObject;
+		// if it was an Apple destroy it
+		if (collidedWith.tag == "Apple")
+			Destroy (collidedWith);
+	}
+}
