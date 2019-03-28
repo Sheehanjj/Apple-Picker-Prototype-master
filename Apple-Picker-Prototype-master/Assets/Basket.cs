6495279afb83e6badfba388efd961291c39d66d6
John Sheehan <sheehanj@sunyulster.edju>
2019-03-28 11:01:32 -0400

    Add Scorekeeping

--- Apple-Picker-Prototype-master/Assets/Basket.cs
+++ Apple-Picker-Prototype-master/Assets/Basket.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Basket : MonoBehaviour {
 
+	public Text scoreGT;
+
 	// Use this for initialization
 	void Start () {
-		
+		GameObject scoreGO = GameObject.Find ("ScoreCounter");
+		scoreGT = scoreGO.GetComponent<Text> ();
+		scoreGT.text = "0";
 	}
 	
 	// Update is called once per frame
@@ -33,5 +38,10 @@ public class Basket : MonoBehaviour {
 		// if it was an Apple destroy it
 		if (collidedWith.tag == "Apple")
 			Destroy (collidedWith);
+
+		int score = int.Parse (scoreGT.text);
+		score += 100; // add 100 points
+		scoreGT.text = score.ToString();
+
 	}
 }
